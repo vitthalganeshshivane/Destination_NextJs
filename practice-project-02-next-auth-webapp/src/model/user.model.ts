a9dc3e0d1a661849803c9343ebbc0d2055349633
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-19 19:54:39 +0530

    feat: add user model

---
+++ practice-project-02-next-auth-webapp/src/model/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  _id?: mongoose.Types.ObjectId;
+  name?: string;
+  image?: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models?.User || mongoose.model("User", userSchema);
+export default User;
