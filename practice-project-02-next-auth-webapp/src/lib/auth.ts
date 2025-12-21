016b42b4a969251d087b29c288477e5d9361a090
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-21 23:46:24 +0530

    feat(auth): implement credentials-based authentication with JWT session

---
+++ practice-project-02-next-auth-webapp/src/lib/auth.ts
@@ -0,0 +1,80 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDb from "./db";
+import User from "@/model/user.model";
+import bcrypt from "bcryptjs";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        if (!email || !password) {
+          throw new Error("email or password is not found");
+        }
+
+        await connectDb();
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new Error("user not found");
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+
+        if (!isMatched) {
+          throw new Error("Incorrect Password");
+        }
+
+        return {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          image: user?.image,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.image = user.image;
+      }
+      return token;
+    },
+    // session
+
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.image as string;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+};
+
+export default authOptions;
