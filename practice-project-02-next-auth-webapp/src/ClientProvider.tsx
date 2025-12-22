53f5ef1b201e0b627a73a3accdb691f5b30b34c9
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-22 23:44:02 +0530

    feat(auth): add client-side SessionProvider wrapper

---
+++ practice-project-02-next-auth-webapp/src/ClientProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+function ClientProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <SessionProvider>{children}</SessionProvider>
+    </div>
+  );
+}
+
+export default ClientProvider;
