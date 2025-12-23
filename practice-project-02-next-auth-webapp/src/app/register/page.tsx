b1a618a0dc5761da334fd06b3b845d0afc396fef
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-24 00:45:16 +0530

    feat(auth): wire register page with API and Google sign-up

--- practice-project-02-next-auth-webapp/src/app/register/page.tsx
+++ practice-project-02-next-auth-webapp/src/app/register/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import axios from "axios";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 
@@ -7,11 +11,30 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const router = useRouter();
+  const session = useSession();
+
+  // console.log(session);
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-black text-white px-4">
       <div className="w-full max-w-md border-2 border-white rounded-2xl p-8 shadow-8 bg-gray-900">
         <h1 className="text-2xl font-semibold text-center mb-6">Register</h1>
-        <form>
+        <form className="space-y-6" onSubmit={handleRegister}>
           <div>
             <label className="block font-medium">Name</label>
             <input
@@ -47,7 +70,12 @@ function Register() {
 
           <p className="text-sm text-center mt-1 mb-4">
             Already have an account ?{" "}
-            <span className="text-blue-400 hover:underline">login</span>
+            <span
+              className="text-blue-400 hover:underline cursor-pointer"
+              onClick={() => router.push("/login")}
+            >
+              Login
+            </span>
           </p>
 
           <button className="w-full py-2 px-4 mb-3 bg-white text-black font-semibold rounded-lg hover:bg-gray-300 transition-colors">
@@ -61,7 +89,12 @@ function Register() {
           <hr className="flex-grow border-gray-500" />
         </div>
 
-        <button className="w-full flex items-center justify-center gap-2 py-2 px-4 border border-gray-400 rounded-lg bg-white text-black hover:bg-gray-300 transition-colors">
+        <button
+          className="w-full flex items-center justify-center gap-2 py-2 px-4 border border-gray-400 rounded-lg bg-white text-black hover:bg-gray-300 transition-colors"
+          onClick={async () => {
+            await signIn("google", { callbackUrl: "/" });
+          }}
+        >
           <FcGoogle />
           <span>Sign Up With Google</span>
         </button>
